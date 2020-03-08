2f67902f1fdb2f4e0803d33fed130c565e07c8d0
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-03-08 22:15:47 +0200

    Updates

---
+++ precache-manifest.c353faeb4337b1678b92956fc3dc9a1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bbb2e8c2f73cfefbe63a7a31f16e34",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "06f26ee697f1a1f8da32",
+    "url": "/weather-app/static/css/main.96c163f7.chunk.css"
+  },
+  {
+    "revision": "c3f8841dd3b0d6ac0df8",
+    "url": "/weather-app/static/js/2.7cc36e60.chunk.js"
+  },
+  {
+    "revision": "9903440051e26cc6d85d7de45bb7193b",
+    "url": "/weather-app/static/js/2.7cc36e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f26ee697f1a1f8da32",
+    "url": "/weather-app/static/js/main.d2a14a97.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  },
+  {
+    "revision": "dcbdcd7f416c1de4f5a16f866c7f1fb4",
+    "url": "/weather-app/static/media/spinner.dcbdcd7f.svg"
+  }
+]);
